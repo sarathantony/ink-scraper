af870437b3f9b290daae2a9a29d859ea1819ffdb
Sarath Antony <sarathantony@yahoo.co.in>
2025-11-23 10:08:49 +0530

    fix: Fix the year in the date(dd/mm/yyyy) matching(4 digits as per the regex) and being inserted as data for the results.

--- src/lib/scraper.js
+++ src/lib/scraper.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { PDFParse } = require('pdf-parse');
-const { extractNumbers } = require('../utils/common.utils');
+const { PDFParse } = require("pdf-parse");
+const { extractNumbers } = require("../utils/common.utils");
 
 async function getResults(serialNumber) {
   const url = `${process.env.TARGET_URL}${serialNumber}`;
@@ -15,38 +15,43 @@ async function getResults(serialNumber) {
     }
 
     const text = result.text.replace(/\s+/g, " ").trim();
+    // Remove the date occurances as it could be a match(four digits) for year in dd/mm/yyyy.
+    const sanitizedText = text.replace(
+      /\b\d{1,2}\/\d{1,2}\/\d{4}\s+\d{1,2}:\d{2}:\d{2}\s+--\s+\d+\s+of\s+\d+\s+--/g,
+      ""
+    );
     const data = {
       serialNumber:
-        text.match(/LOTTERY\s+NO\.([A-Z]{1,3}-\d+)/i)?.[1].replace("-", "") ||
+        sanitizedText.match(/LOTTERY\s+NO\.([A-Z]{1,3}-\d+)/i)?.[1].replace("-", "") ||
         null,
-      date: text.match(/\b\d{2}\/\d{2}\/\d{4}\b/)?.[0] || null,
+      date: sanitizedText.match(/\b\d{2}\/\d{2}\/\d{4}\b/)?.[0] || null,
       fiveThousand: extractNumbers(
-        text,
+        sanitizedText,
         /4th\s*Prize[-\s]*Rs\s*:?5000\/?-?\s*(.*?)5th\s*Prize/,
         /3rd Prize[-\s]*Rs\s*:?5000\/?-?\s*(.*?)4th Prize/
       ),
       twoThousand: extractNumbers(
-        text,
+        sanitizedText,
         /5th\s*Prize[-\s]*Rs\s*:?2000\/?-?\s*(.*?)6th\s*Prize/,
         /4th Prize[-\s]*Rs\s*:?2000\/?-?\s*(.*?)5th Prize/
       ),
       thousand: extractNumbers(
-        text,
+        sanitizedText,
         /6th\s*Prize[-\s]*Rs\s*:?1000\/?-?\s*(.*?)7th\s*Prize/,
         /5th Prize[-\s]*Rs\s*:?1000\/?-?\s*(.*?)6th Prize/
       ),
       fiveHundred: extractNumbers(
-        text,
+        sanitizedText,
         /7th\s*Prize[-\s]*Rs\s*:?500\/?-?\s*(.*?)8th\s*Prize/,
         /6th Prize[-\s]*Rs\s*:?500\/?-?\s*(.*?)7th Prize/
       ),
       twoHundred: extractNumbers(
-        text,
+        sanitizedText,
         /8th\s*Prize[-\s]*Rs\s*:?200\/?-?\s*(.*?)9th\s*Prize/,
         /7th Prize[-\s]*Rs\s*:?200\/?-?\s*(.*?)8th Prize/
       ),
       hundred: extractNumbers(
-        text,
+        sanitizedText,
         /9th\s*Prize[-\s]*Rs\s*:?100\/?-?\s*(.*?)The prize winners/i,
         /8th Prize[-\s]*Rs\s*:?100\/?-?\s*(.*?)The prize winners/i
       ),
@@ -58,4 +63,4 @@ async function getResults(serialNumber) {
   }
 }
 
-module.exports = { getResults }
+module.exports = { getResults };
